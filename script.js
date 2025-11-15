871cb9b2cace823705460197a5939ce6838a4db3
unknown <rajatul8496@gmail.com>
2025-11-15 18:52:36 +0530

    updated script.js

--- script.js
+++ script.js
@@ -8,6 +8,26 @@
 
 //  git commit -m ('something') -> it is used to save a check point or your changes to the local repository with a message describing what changes were made.
 
+
+// commands to do : 
+// 1. git init -> to initialize a git repository in your project folder
+// 2. git add . -> to stage all the changes made in the project for the next commit
+// 3, git commit -m "commit name" -> creating checkpoint
+// 4.git remote add origin https://github.com/AtulRaj37/demo1.git -> adding remote repository (address)
+// 5. git push -u origin main -> pushing your local commits to the remote repository on the main branch
 function sum(a, b) {
     return a + b;
 }
+
+// git clone <repository_url> -> to copy a remote repository to your local machine
+// git pull -> to fetch and merge changes from the remote repository to your local repository
+// git status -> to check the status of your working directory and staging area
+// git log -> to view the commit history of the repository
+// git checkout <branch_name> -> to switch to a different branch
+
+now to send the change the collaborated code from cloud ,one need to :
+    1. git add . 2, git commit -m "second commit" 3. git push
+
+now "git pull" is used to get the changed code from github
+
+after that use " git push" for your changes to be reflected on github
