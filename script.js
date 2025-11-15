022c8ee055848256b7ddaf95f60f9ef24a40864d
unknown <rajatul8496@gmail.com>
2025-11-15 12:43:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// what is git ?
+
+// Git is a distributed version control system that allows multiple people to work on a project simultaneously without interfering with each other's changes. It tracks changes to files and directories over time, enabling users to revert to previous versions, collaborate on code, and manage different branches of development.
+//  Git is widely used in software development for source code management and is the underlying technology behind platforms like GitHub and GitLab.
+
+//  In simple words , git does version control of files ki unka alag alag checkpoints pr unka content kaisa tha and kon kon usme edit kiye.
+//  Git helps you to save different versions of your code and collaborate with others easily. 
+
+//  git commit -m ('something') -> it is used to save a check point or your changes to the local repository with a message describing what changes were made.
+
+function sum(a, b) {
+    return a + b;
+}
